8808083bd30de16723a734f1b76e50c9d03b8f40
AndreiKirillov <AndreiKirillov@users.noreply.github.com>
2022-03-08 22:51:55 +0300

    AddModel

---
+++ HelloWeb/Data/MvcStudentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HelloWeb.Models;
+
+namespace HelloWeb.Data
+{
+    public class MvcStudentContext : DbContext
+    {
+        public MvcStudentContext(DbContextOptions<MvcStudentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+    }
+}
--- HelloWeb/Startup.cs
+++ HelloWeb/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using HelloWeb.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace HelloWeb
 {
@@ -24,6 +26,9 @@ namespace HelloWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcStudentContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("MvcStudentContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
